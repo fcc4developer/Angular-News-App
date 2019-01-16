1321420b69347bb87fa7fc006fd701a263e72fb5
Dragana <dragana.ilievska10@gmail.com>
2019-01-16 13:43:17 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MaterialModule } from './material.module';
+// import { FlexLayoutModule } from '@angular/flex-layout';
+// tslint:disable-next-line:max-line-length
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatSelectModule, MatProgressSpinnerModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { ServiceWorkerModule } from '@angular/service-worker';
+import { NewsApiService } from './news-api.service';
 import { AppComponent } from './app.component';
+// import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,9 +21,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    // MaterialModule,
+    // FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
